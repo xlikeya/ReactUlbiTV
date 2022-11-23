bea94cdcfc9af156bb89c1ac224a5bc47f84de58
xlikeya <shpanda91@gmail.com>
2022-11-23 16:22:38 +0700

    changes 23.11.22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import PostItem from "./components/PostItem";
+import './styles/App.css'
 
 function App() {
+    const [posts,setPost] = useState([
+        {id: 1, title: 'Javascript', body: 'Description'},
+        {id: 2, title: 'Python', body: 'Description'},
+        {id: 3, title: 'Ruby', body: 'Description'},
+        {id: 4, title: 'Java', body: 'Description'},
+        {id: 5, title: 'Scala', body: 'Description'},
+        {id: 6, title: 'HTML о.О', body: 'Description'},
+    ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {posts.map(post =>
+            <PostItem post={post}></PostItem>
+        )}
     </div>
   );
 }
