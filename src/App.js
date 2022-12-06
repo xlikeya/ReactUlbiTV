05c6eaa449c242b61f40ccdb6930306c150a5ed7
vera <shpanda91@gmail.com>
2022-12-06 22:54:12 +0700

    Add Loader

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
 
 function App() {
     const [posts,setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [isPostsLoading, setIsPostsLoading] = useState(false)
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -21,9 +23,12 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+
     async function fetchPosts() {
+        setIsPostsLoading(true);
         const posts = await PostService.getAll();
         setPosts(posts)
+        setIsPostsLoading(false)
     }
 
     useEffect(() => {
@@ -42,7 +47,11 @@ function App() {
             filter = {filter}
             setFilter={setFilter}
         ></PostFilter>
-            <PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Мой список"}></PostList>
+        {isPostsLoading
+            ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader></Loader></div>
+            :<PostList remove={removePost} posts={sortedAndSearchedPosts} title={"Мой список"}></PostList>
+
+        }
     </div>
   );
 }
