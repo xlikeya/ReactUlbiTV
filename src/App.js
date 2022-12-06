26ea7ecf3115c57b5441afb46f7aa761b1d9388a
vera <shpanda91@gmail.com>
2022-12-06 07:16:50 +0700

    get posts from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import FormPost from "./components/FormPost";
 import PostList from "./components/PostList";
 import './styles/App.css'
@@ -6,19 +6,14 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
 
 function App() {
-    const [posts,setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Description1'},
-        {id: 2, title: 'Python', body: 'Description2'},
-        {id: 3, title: 'Ruby', body: 'Description3'},
-        {id: 4, title: 'Java', body: 'Description4'},
-        {id: 5, title: 'Scala', body: 'Description5'},
-        {id: 6, title: 'HTML о.О', body: 'Description6'},
-    ])
+    const [posts,setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
@@ -26,8 +21,14 @@ function App() {
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
+    async function fetchPosts() {
+        const posts = await PostService.getAll();
+        setPosts(posts)
+    }
 
-
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
   return (
     <div className="App">
