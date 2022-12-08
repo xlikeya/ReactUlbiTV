bc950e46c133aac356790c158c443fd1b68d7114
vera <shpanda91@gmail.com>
2022-12-08 08:07:17 +0700

    pagination

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -3,8 +3,14 @@ import axios from "axios";
 export default class PostService {
     static  async getAll(limit = 10, page = 1){
 
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-            return response;
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+            params: {
+                _limit: limit,
+                _page: page
+            }
+        })
+
+        return response;
 
     }
 }
--- src/App.js
+++ src/App.js
@@ -9,17 +9,21 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPageCount} from "./utils/pages";
 
 function App() {
     const [posts,setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [totalCount, setTotalCount] = useState(0)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostService.getAll();
+        const response = await PostService.getAll(limit, page);
         setPosts(response.data)
-        setTotalCount(response.headers['x-total-count'])
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPageCount(totalCount, limit))
     })
 
 
